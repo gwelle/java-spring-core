1e01612cf922bb375ec4dbda5b951eb6df9b3045
gwelle <guillaume.welle.sio@gmail.com>
2024-04-06 12:00:14 +0200

    injection de dépendance avec un constructeur avec la classe person dependant d'une collectione

--- src/main/java/org/eclipse/model/Person.java
+++ src/main/java/org/eclipse/model/Person.java
@@ -1,20 +1,26 @@
 package org.eclipse.model;
 
+import java.util.List;
+
 public class Person {
 
 	private int id;
 	private String firstName;
 	private String lastName;
 	private Address address;
+	private List<String> sports;
 
-	public Person(int id, String firstName, String lastName, Address address) {
+	public Person(int id, String firstName, String lastName, Address address, List<String> sports) {
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.address = address;
+		this.sports = sports;
 	}
 
 	public void display() {
-		System.out.println(id + " " + firstName + " " + lastName + " " + address);
+		System.out.println(id + " " + firstName + " " + lastName + " " + address + "\n");
+		System.out.println("My sports : ");
+		sports.forEach(System.out::println);
 	}
 }
