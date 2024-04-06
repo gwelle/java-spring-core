62ab057dfe3473e6eb647942cf7cdca1632ac3ee
gwelle <guillaume.welle.sio@gmail.com>
2024-04-06 11:45:38 +0200

    injection de dépendance avec un constructeur

---
+++ src/main/java/org/eclipse/main/Main.java
@@ -0,0 +1,13 @@
+package org.eclipse.main;
+
+import org.eclipse.model.Person;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Person person = context.getBean("per", Person.class);
+		person.display();
+	}
+}
